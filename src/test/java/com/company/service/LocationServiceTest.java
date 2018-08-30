0ded1dd6c6ab5635f8faf18779340a21600c844c
liweixi <liweixi@hansap.com>
2018-08-30 15:17:06 +0800

    修改提交值

--- src/test/java/com/company/service/LocationServiceTest.java
+++ src/test/java/com/company/service/LocationServiceTest.java
@@ -15,7 +15,9 @@ public class LocationServiceTest extends TestCase {
 
     private LocationService locationService;
 
-    private static final String url = "http://localhost:5000";
+    private static final String url = "http://gy.cnrobinson.com";
+    private static final String clientId = "";
+    private static final String secret = "";
     protected void setUp() {
         locationService = new LocationService();
         locationService.setUrl(url);
@@ -28,14 +30,14 @@ public class LocationServiceTest extends TestCase {
      */
     public void testRegisterDriver() throws Exception {
         RegisterDriverRequest driver = new RegisterDriverRequest();
-        driver.setClientId("db57571d53df4fe19213ee6999476d69");
+        driver.setClientId(clientId);
         driver.setTimestamp("15747545455");
         driver.setDriverName("test");
         driver.setIdentityCard("1111111111244545");
         driver.setLdrivingLicense("43545766878");
         driver.setPhone("15210802181");
         driver.setPlateNum("鄂A8936");
-        driver.makeMd5Signature("d3f5af202c229cb5e86675d3695f02a2");
+        driver.makeMd5Signature(secret);
         ObjectRestResponse<AuthRegResponse> response = locationService.registerDriver(driver);
         System.out.println(JSONObject.toJSONString(response));
     }
@@ -47,10 +49,10 @@ public class LocationServiceTest extends TestCase {
      */
     public void testQueryAuthReg() throws Exception {
         PhoneLocationRequest request = new PhoneLocationRequest();
-        request.setClientId("cc2ee95d83424f7988650755b49bca60");
+        request.setClientId(clientId);
         request.setTimestamp("15747545455");
         request.setPhone("15210802181");
-        request.makeMd5Signature("581f408d83d0de4d7a6b312d8909fa24");
+        request.makeMd5Signature(secret);
         ObjectRestResponse<AuthRegQueryResponse> response = locationService.queryAuthReg(request);
         System.out.println(JSONObject.toJSONString(response));
     }
@@ -62,10 +64,10 @@ public class LocationServiceTest extends TestCase {
      */
     public void testSmsLocation() throws Exception {
         PhoneLocationRequest request = new PhoneLocationRequest();
-        request.setClientId("cc2ee95d83424f7988650755b49bca60");
+        request.setClientId(clientId);
         request.setTimestamp("15747545455");
         request.setPhone("15210802181");
-        request.makeMd5Signature("581f408d83d0de4d7a6b312d8909fa24");
+        request.makeMd5Signature(secret);
         ObjectRestResponse<SmsLocationResponse> response = locationService.smsLocation(request);
         System.out.println(JSONObject.toJSONString(response));
     }
@@ -78,9 +80,9 @@ public class LocationServiceTest extends TestCase {
     public void testPlatenumLocation() throws Exception {
         PlatenumRequest request = new PlatenumRequest();
         request.setPlatenum("黑F88739");
-        request.setClientId("db57571d53df4fe19213ee6999476d69");
+        request.setClientId(clientId);
         request.setTimestamp("15747545455");
-        request.makeMd5Signature("d3f5af202c229cb5e86675d3695f02a2");
+        request.makeMd5Signature(secret);
         ObjectRestResponse<PlatenumLocation> response = locationService.platenumLocation(request);
         System.out.println(JSONObject.toJSONString(response));
     }
